9d3eacc833d1cb5e508de68706dfddd838b79baa
Naveen Jammula <noblenaveen@gmail.com>
2018-10-25 13:47:06 -0500

    Like tabs with mute/UnMute options

---
+++ Constants.js
@@ -0,0 +1,6 @@
+export const RELOAD_CONTEXTMENU_ITEM_ID = "Reload_ID";
+export const RELOAD_CONTEXTMENU_ITEM_TITLE = "Reload";
+
+export const CLOSE_CONTEXTMENU_ITEM_ID = "Close_ID";
+export const CLOSE_CONTEXTMENU_ITEM_TITLE = "Close";
+
--- background.js
+++ background.js
@@ -1,62 +1,78 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
-
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log('The color is green.');
   });
 });
 
+const RELOAD_CONTEXTMENU_ITEM_ID = "Reload_ID";
+const RELOAD_CONTEXTMENU_ITEM_TITLE = "Reload";
+
+const CLOSE_CONTEXTMENU_ITEM_ID = "Close_ID";
+const CLOSE_CONTEXTMENU_ITEM_TITLE = "Close";
+
+const MUTE_CONTEXTMENU_ITEM_ID = "Mute_Id";
+const MUTE_CONTEXTMENU_ITEM_TITLE = "Mute";
+
+const UNMUTE_CONTEXTMENU_ITEM_ID = "Unmute_Id";
+const UNMUTE_CONTEXTMENU_ITEM_TITLE = "Unmute";
+
 
 chrome.contextMenus.create({
-  id:"Reload_Like_Tabs",
-  title: "Reload Like Tabs",
-  type:"normal",
+  id:CLOSE_CONTEXTMENU_ITEM_ID,
+  title: CLOSE_CONTEXTMENU_ITEM_TITLE,
+  type:"normal"
 },function(){
   buildNestedContextMenuItems();
 });
 
 function buildNestedContextMenuItems(){
   chrome.contextMenus.create({
-    id:"Close_Like_Tabs",
-    title: "Close Like Tabs",
-    type:"normal"
+    id: RELOAD_CONTEXTMENU_ITEM_ID,
+    title: RELOAD_CONTEXTMENU_ITEM_TITLE,
+    type:"normal",
+  });
+  chrome.contextMenus.create({
+    id: MUTE_CONTEXTMENU_ITEM_ID,
+    title: MUTE_CONTEXTMENU_ITEM_TITLE,
+    type:"normal",
+  });
+  chrome.contextMenus.create({
+    id: UNMUTE_CONTEXTMENU_ITEM_ID,
+    title: UNMUTE_CONTEXTMENU_ITEM_TITLE,
+    type:"normal",
   });
 }
 
-chrome.contextMenus.onClicked.addListener(function(clickTaggedImage, tab) {
-  var currentTab = tab;
-  if (clickTaggedImage.menuItemId === "Close_Like_Tabs") {
-    chrome.windows.getAll({populate:true},function(windows){
-      windows.forEach(function(window){
-        window.tabs.forEach(function(tab){
-          var url = new URL(tab.url);
-          var currentTabUrl = new URL(currentTab.url);
-          var currentTabHostName = currentTabUrl.hostname;
-          if(url.hostname === currentTabHostName){
+function ContextMenuLinkActions(tab, currentTab, contextMenuItemId){
+  chrome.windows.getAll({populate:true},function(windows){
+    windows.forEach(function(window){
+      window.tabs.forEach(function(tab){
+        var url = new URL(tab.url);
+        var currentTabUrl = new URL(currentTab.url);
+        var currentTabHostName = currentTabUrl.hostname;
+        if(url.hostname === currentTabHostName){
+          if(contextMenuItemId === CLOSE_CONTEXTMENU_ITEM_ID){
             chrome.tabs.remove(tab.id);
           }
-        });
-      });
-    });
-  }
-
-  if (clickTaggedImage.menuItemId === "Reload_Like_Tabs") {
-    chrome.windows.getAll({populate:true},function(windows){
-      windows.forEach(function(window){
-        window.tabs.forEach(function(tab){
-          var url = new URL(tab.url);
-          var currentTabUrl = new URL(currentTab.url);
-          var currentTabHostName = currentTabUrl.hostname;
-          if(url.hostname === currentTabHostName){
+          if(contextMenuItemId === RELOAD_CONTEXTMENU_ITEM_ID){
             chrome.tabs.reload(tab.id);
           }
-        });
+          if(contextMenuItemId === MUTE_CONTEXTMENU_ITEM_ID){
+            chrome.tabs.update(tab.id,{muted: true});
+          }
+          if(contextMenuItemId === UNMUTE_CONTEXTMENU_ITEM_ID){
+            chrome.tabs.update(tab.id,{muted: false});
+          }
+        }
       });
     });
-  }
-  
+  });
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickedContextMenu, tab) {
+  var currentTab = tab;
+  ContextMenuLinkActions(tab,currentTab,clickedContextMenu.menuItemId);
 });
+
+
