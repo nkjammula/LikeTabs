9eff665f224d66307c94ec599b1611936cfd56a0
Naveen Jammula <Noblenaveen@gmail.com>
2018-11-01 17:01:58 -0500

    Like tabs including close other tabs option

--- background.js
+++ background.js
@@ -5,17 +5,20 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
-const RELOAD_CONTEXTMENU_ITEM_ID = "Reload_ID";
-const RELOAD_CONTEXTMENU_ITEM_TITLE = "Reload";
+const RELOAD_ALL_CONTEXTMENU_ITEM_ID = "ReloadAll_ID";
+const RELOAD_ALL_CONTEXTMENU_ITEM_TITLE = "Reload All";
 
 const CLOSE_CONTEXTMENU_ITEM_ID = "Close_ID";
-const CLOSE_CONTEXTMENU_ITEM_TITLE = "Close";
+const CLOSE_CONTEXTMENU_ITEM_TITLE = "Close other tabs";
 
-const MUTE_CONTEXTMENU_ITEM_ID = "Mute_Id";
-const MUTE_CONTEXTMENU_ITEM_TITLE = "Mute";
+const CLOSE_ALL_CONTEXTMENU_ITEM_ID = "CloseAll_ID";
+const CLOSE_ALL_CONTEXTMENU_ITEM_TITLE = "Close All";
 
-const UNMUTE_CONTEXTMENU_ITEM_ID = "Unmute_Id";
-const UNMUTE_CONTEXTMENU_ITEM_TITLE = "Unmute";
+const MUTE_ALL_CONTEXTMENU_ITEM_ID = "MuteAll_Id";
+const MUTE_ALL_CONTEXTMENU_ITEM_TITLE = "Mute All";
+
+const UNMUTE_ALL_CONTEXTMENU_ITEM_ID = "UnmuteAll_Id";
+const UNMUTE_ALL_CONTEXTMENU_ITEM_TITLE = "Unmute All";
 
 const DUPLICATE_CONTEXTMENU_ITEM_ID = "Duplicate_Id";
 const DUPLICATE_CONTEXTMENU_ITEM_TITLE = "Duplicate";
@@ -28,8 +31,8 @@ const UNPIN_CONTEXTMENU_ITEM_TITLE = "UnPin";
 
 
 chrome.contextMenus.create({
-  id:CLOSE_CONTEXTMENU_ITEM_ID,
-  title: CLOSE_CONTEXTMENU_ITEM_TITLE,
+  id:CLOSE_ALL_CONTEXTMENU_ITEM_ID,
+  title: CLOSE_ALL_CONTEXTMENU_ITEM_TITLE,
   type:"normal"
 },function(){
   buildNestedContextMenuItems();
@@ -37,35 +40,40 @@ chrome.contextMenus.create({
 
 function buildNestedContextMenuItems(){
   chrome.contextMenus.create({
-    id: RELOAD_CONTEXTMENU_ITEM_ID,
-    title: RELOAD_CONTEXTMENU_ITEM_TITLE,
+    id: CLOSE_CONTEXTMENU_ITEM_ID,
+    title: CLOSE_CONTEXTMENU_ITEM_TITLE,
     type:"normal",
   });
   chrome.contextMenus.create({
-    id: MUTE_CONTEXTMENU_ITEM_ID,
-    title: MUTE_CONTEXTMENU_ITEM_TITLE,
+    id: RELOAD_ALL_CONTEXTMENU_ITEM_ID,
+    title: RELOAD_ALL_CONTEXTMENU_ITEM_TITLE,
     type:"normal",
   });
   chrome.contextMenus.create({
-    id: UNMUTE_CONTEXTMENU_ITEM_ID,
-    title: UNMUTE_CONTEXTMENU_ITEM_TITLE,
+    id: MUTE_ALL_CONTEXTMENU_ITEM_ID,
+    title: MUTE_ALL_CONTEXTMENU_ITEM_TITLE,
     type:"normal",
   });
   chrome.contextMenus.create({
-    id: DUPLICATE_CONTEXTMENU_ITEM_ID,
-    title: DUPLICATE_CONTEXTMENU_ITEM_TITLE,
+    id: UNMUTE_ALL_CONTEXTMENU_ITEM_ID,
+    title: UNMUTE_ALL_CONTEXTMENU_ITEM_TITLE,
     type:"normal",
   });
-  chrome.contextMenus.create({
-    id: PIN_CONTEXTMENU_ITEM_ID,
-    title: PIN_CONTEXTMENU_ITEM_TITLE,
-    type:"normal",
-  });
-  chrome.contextMenus.create({
-    id: UNPIN_CONTEXTMENU_ITEM_ID,
-    title: UNPIN_CONTEXTMENU_ITEM_TITLE,
-    type:"normal",
-  });
+  // chrome.contextMenus.create({
+  //   id: DUPLICATE_CONTEXTMENU_ITEM_ID,
+  //   title: DUPLICATE_CONTEXTMENU_ITEM_TITLE,
+  //   type:"normal",
+  // });
+  // chrome.contextMenus.create({
+  //   id: PIN_CONTEXTMENU_ITEM_ID,
+  //   title: PIN_CONTEXTMENU_ITEM_TITLE,
+  //   type:"normal",
+  // });
+  // chrome.contextMenus.create({
+  //   id: UNPIN_CONTEXTMENU_ITEM_ID,
+  //   title: UNPIN_CONTEXTMENU_ITEM_TITLE,
+  //   type:"normal",
+  // });
 }
 
 function ContextMenuLinkActions(tab, currentTab, contextMenuItemId){
@@ -76,27 +84,30 @@ function ContextMenuLinkActions(tab, currentTab, contextMenuItemId){
         var currentTabUrl = new URL(currentTab.url);
         var currentTabHostName = currentTabUrl.hostname;
         if(url.hostname === currentTabHostName){
-          if(contextMenuItemId === CLOSE_CONTEXTMENU_ITEM_ID){
+          if(contextMenuItemId === CLOSE_ALL_CONTEXTMENU_ITEM_ID){
             chrome.tabs.remove(tab.id);
           }
-          if(contextMenuItemId === RELOAD_CONTEXTMENU_ITEM_ID){
+          if(contextMenuItemId === RELOAD_ALL_CONTEXTMENU_ITEM_ID){
             chrome.tabs.reload(tab.id);
           }
-          if(contextMenuItemId === MUTE_CONTEXTMENU_ITEM_ID){
+          if(contextMenuItemId === CLOSE_CONTEXTMENU_ITEM_ID && currentTab.id != tab.id ){
+            chrome.tabs.remove(tab.id);
+          }
+          if(contextMenuItemId === MUTE_ALL_CONTEXTMENU_ITEM_ID){
             chrome.tabs.update(tab.id,{muted: true});
           }
-          if(contextMenuItemId === UNMUTE_CONTEXTMENU_ITEM_ID){
+          if(contextMenuItemId === UNMUTE_ALL_CONTEXTMENU_ITEM_ID){
             chrome.tabs.update(tab.id,{muted: false});
           }
-          if(contextMenuItemId === DUPLICATE_CONTEXTMENU_ITEM_ID){
-            chrome.tabs.duplicate(currentTab.id);
-          }
-          if(contextMenuItemId === PIN_CONTEXTMENU_ITEM_ID){
-            chrome.tabs.update(tab.id,{pinned: true});
-          }
-          if(contextMenuItemId === UNPIN_CONTEXTMENU_ITEM_ID){
-            chrome.tabs.update(tab.id,{pinned: false});
-          }
+          // if(contextMenuItemId === DUPLICATE_CONTEXTMENU_ITEM_ID){
+          //   chrome.tabs.duplicate(currentTab.id);
+          // }
+          // if(contextMenuItemId === PIN_CONTEXTMENU_ITEM_ID){
+          //   chrome.tabs.update(tab.id,{pinned: true});
+          // }
+          // if(contextMenuItemId === UNPIN_CONTEXTMENU_ITEM_ID){
+          //   chrome.tabs.update(tab.id,{pinned: false});
+          // }
         }
       });
     });
