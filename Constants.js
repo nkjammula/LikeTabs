ccfe978eb88fb0956cd181eed58ff452cdc2380a
Naveen Jammula <Noblenaveen@gmail.com>
2018-11-02 13:30:21 -0500

    Like tabs, Images

--- Constants.js
+++
@@ -1,6 +0,0 @@
-export const RELOAD_CONTEXTMENU_ITEM_ID = "Reload_ID";
-export const RELOAD_CONTEXTMENU_ITEM_TITLE = "Reload";
-
-export const CLOSE_CONTEXTMENU_ITEM_ID = "Close_ID";
-export const CLOSE_CONTEXTMENU_ITEM_TITLE = "Close";
-
