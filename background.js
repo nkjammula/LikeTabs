e7a03b305479cc760aed9ed4398435dd5fe21771
Naveen Jammula <Noblenaveen@gmail.com>
2018-11-28 15:45:49 -0600

    With pop up menu

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 'use strict';
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-});
+// chrome.runtime.onInstalled.addListener(function() {
+//   chrome.storage.sync.set({color: '#3aa757'}, function() {
+//     console.log('The color is green.');
+//   });
+// });
 
 const RELOAD_ALL_CONTEXTMENU_ITEM_ID = "ReloadAll_ID";
 const RELOAD_ALL_CONTEXTMENU_ITEM_TITLE = "Reload All";
